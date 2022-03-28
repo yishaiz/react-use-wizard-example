15bb835817328573e3032cf6fe6ca2dd5213790d
yishaiza <Yishai.Zaltzberg@samanage.com>
2022-03-28 11:32:01 +0300

    04-is-frist-is-last-goto-last

--- src/Steps/Step1.js
+++ src/Steps/Step1.js
@@ -1,8 +1,20 @@
 import { useWizard } from 'react-use-wizard';
+import { useState } from 'react';
 
 const Step1 = () => {
-  const { handleStep, previousStep, nextStep } = useWizard();
+  const {
+    isLoading,
+    isLastStep,
+    isFirstStep,
+    activeStep,
+    stepCount,
+    previousStep,
+    nextStep,
+    goToStep,
+    handleStep,
+  } = useWizard();
 
+  const [data, setData] = useState('');
   handleStep(() => {
     alert('Going to step 2');
   });
@@ -10,11 +22,23 @@ const Step1 = () => {
   return (
     <>
       <h2>Step 1</h2>
-      <input type='text' width='100px' />
+      {isLoading && <p>loading...</p>}
+      <input
+        type='text'
+        width='100px'
+        value={data}
+        onChange={(event) => setData(event.target.value)}
+      />
       <hr />
       <button onClick={() => previousStep()}>Previous</button>
-
       <button onClick={() => nextStep()}>Next</button>
+      <button onClick={() => goToStep(2)}>Go to the last step</button>
+      <div>
+        Has next step: {!isLastStep ? '✅' : '⛔'}
+        <br />
+        Has previous step : {!isFirstStep ? '✅' : '⛔'}
+      </div>
+      Active step: {activeStep + 1} <br />
     </>
   );
 };
